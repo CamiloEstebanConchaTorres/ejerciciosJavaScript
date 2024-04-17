e1c29923088112eedd7ee234825dcc3446f198ad
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com.>
2024-04-17 17:24:36 -0500

    feat: :sparkles: ejercicio 24 terminado
    se realizo el ejercicio 24 usando metodos de javascript como .filter y tolowercase y startswith

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 
+////////////////////////////////// EJERCICIO 01 CONTRASEÑA/////////////////////////////////////////
+
+  // import prompt from "prompt-sync"
+  // import contrasenaVAlida from "./modules/contrasena.js"
+  // let input = prompt();
+  // let opc = undefined;
+
+
+  // do{
+  //   let data = input("ingrese la contrasena: ");
+  //   console.log(contrasenaVAlida(data));
+  //   console.log('1. Repetir la validacion\n0. Terminar');
+  //   opc = Number(input()); 
+  // }while(opc)
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  ///////////////////////////////////////EJERCICIO 24 PALABRAS CON A////////////////////////////////
 
   import prompt from "prompt-sync"
-  import contrasenaVAlida from "./modules/contrasena.js"
-  let input = prompt();
-  let opc = undefined;
+  import {empiezanConA} from "./modules/ejercicio24.js"
+  const palabras = ['beta', 'alfa', 'Arbol', 'Gamma', 'Apricot', 'orange'];
+  const palabrasQueEmpiezanConA = empiezanConA(palabras);
+  console.log(palabrasQueEmpiezanConA);
 
-
-  do{
-    let data = input("ingrese la contrasena: ");
-    console.log(contrasenaVAlida(data));
-    console.log('1. Repetir la validacion\n0. Terminar');
-    opc = Number(input()); 
-  }while(opc)
-
-
-
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////7
---
+++ js/modules/ejercicio24.js
@@ -0,0 +1,4 @@
+export function empiezanConA(arr) {
+    return arr.filter(palabra => palabra.toLowerCase().startsWith('a'));
+}
+
