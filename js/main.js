fdbd4d9ceef9d2fc1f3c5b626d256ee5543309c5
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-04-18 13:03:44 -0500

    feat: :construction: mezcla de JavaScript con Html y Css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-
-// import prompt from "prompt-sync";
-
-// let app = prompt();
-// console.log("how many more times?");
-// let n = app('');
-
-
-  import prompt from "prompt-sync"
-
-  let input = prompt();
-  let nombre = input('Ingrese el nombre: ');
-  let edad = Number(input('Ingrese la edad: '));
-  console.log(nombre);
-  console.log(edad);
-
-
+let myfortm = document.querySelector(`[name="nombre"]`);
+console.log(myfortm);
 
 
